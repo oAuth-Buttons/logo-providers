67b77c2f68329c9e005dd415dcd72a151b56a027
Ranol <asdhugh1@gmail.com>
2018-05-15 22:41:25 +0900

    Rename LocalLogoProvider to UrlLogoProvider

--- src/DefaultLogoProviders.ts
+++ src/DefaultLogoProviders.ts
@@ -1,15 +1,15 @@
 import { LogoProvider, Logo } from './LogoProviders';
 
-export class LocalLogoProvider implements LogoProvider {
+export class UrlLogoProvider implements LogoProvider {
     name: 'LocalLogoProvider';
-    private fileList: string[];
+    private readonly urlList: string[];
 
     constructor(fileList: string[]) {
-        this.fileList = fileList;
+        this.urlList = fileList;
     }
 
     provide(id: string): Logo | null {
-        for (let file of this.fileList) {
+        for (let file of this.urlList) {
             let dot = file.lastIndexOf('.');
             let name = file;
             if (dot !== -1) {
