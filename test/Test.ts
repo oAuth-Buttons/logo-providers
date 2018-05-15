ecb13f8fec9a5049613bc97ffd0f477d79df4ff9
Ranol <asdhugh1@gmail.com>
2018-05-15 22:56:31 +0900

    Add DefaultLogoProvider and update test cases

--- test/Test.ts
+++ test/Test.ts
@@ -1,20 +1,17 @@
-import { UrlLogoProvider } from '../src/DefaultLogoProviders';
-import { Logo, ThemeColorStorage } from '../src/LogoProviders';
+import { DefaultLogoProvider } from '../src/DefaultLogoProviders';
+import { ThemeColorStorage } from '../src/LogoProviders';
 
 import { expect } from 'chai';
-
-const logoProvider = new UrlLogoProvider([
-    '../svg/discord.svg',
-    '../svg/facebook.svg',
-    '../svg/github.svg',
-    '../svg/google.svg',
-    '../svg/instagram.svg',
-]);
+import * as Path from 'path';
 
 it('works with logo provider', () => {
-    expect(logoProvider.provide('discord').url).to.be.equal('../svg/discord.svg');
-    expect(logoProvider.provide('google').url).to.be.equal('../svg/google.svg');
-    expect(logoProvider.provide('naver')).to.be.equal(null);
+    let discord = Path.normalize(DefaultLogoProvider.provide('discord').url);
+    expect(discord).to.be.equal(Path.resolve(__dirname, '../src/svg/discord.svg'));
+
+    let google = Path.normalize(DefaultLogoProvider.provide('google').url);
+    expect(google).to.be.equal(Path.resolve(__dirname, '../src/svg/google.svg'));
+
+    expect(DefaultLogoProvider.provide('unset')).to.be.equal(null);
 });
 
 it('works with theme storage', () => {
